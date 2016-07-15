37ce9e71060165f5290057b0b7af71b4252fcb46
Grommerin <grommerin@gmail.com>
2016-07-15 15:43:02 +0300

    Косметические изменения в оформлении кода. Отключен Ethernet интерфейс

--- include/configs/mx6q_bars3000_emmc.h
+++ include/configs/mx6q_bars3000_emmc.h
@@ -30,7 +30,7 @@
 #define CONFIG_DISPLAY_BOARDINFO
 #define CONFIG_SYS_L2CACHE_OFF
 
-#define CONFIG_MACH_TYPE	3771
+#define CONFIG_MACH_TYPE            3771
 
 #include <asm/arch/imx-regs.h>
 #include <asm/imx-common/gpio.h>
@@ -41,37 +41,39 @@
 #define CONFIG_REVISION_TAG
 
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		(10 * 1024 * 1024)
+#define CONFIG_SYS_MALLOC_LEN       (10 * 1024 * 1024)
 
 #define CONFIG_BOARD_EARLY_INIT_F
 #define CONFIG_MISC_INIT_R
 #define CONFIG_MXC_GPIO
 
 #define CONFIG_MXC_UART
-#define CONFIG_MXC_UART_BASE	       UART2_BASE
+#define CONFIG_MXC_UART_BASE        UART2_BASE
 
 //#define CONFIG_CMD_SF
 #ifdef CONFIG_CMD_SF
 #define CONFIG_SPI_FLASH
 #define CONFIG_SPI_FLASH_SST
 #define CONFIG_MXC_SPI
-#define CONFIG_SF_DEFAULT_BUS  0
-#define CONFIG_SF_DEFAULT_CS   (0|(IMX_GPIO_NR(3, 19)<<8))
-#define CONFIG_SF_DEFAULT_SPEED 25000000
-#define CONFIG_SF_DEFAULT_MODE (SPI_MODE_0)
+#define CONFIG_SF_DEFAULT_BUS       0
+#define CONFIG_SF_DEFAULT_CS        (0|(IMX_GPIO_NR(3, 19)<<8))
+#define CONFIG_SF_DEFAULT_SPEED     25000000
+#define CONFIG_SF_DEFAULT_MODE      (SPI_MODE_0)
 #endif
 
 /* I2C Configs */
 //#define CONFIG_CMD_I2C
-//#define CONFIG_I2C_MULTI_BUS
-//#define CONFIG_I2C_MXC
-//#define CONFIG_SYS_I2C_SPEED		100000
+#if defined(CONFIG_CMD_I2C)
+#define CONFIG_I2C_MULTI_BUS
+#define CONFIG_I2C_MXC
+#define CONFIG_SYS_I2C_SPEED        100000
+#endif
 
 /* MMC Configs */
 #define CONFIG_FSL_ESDHC
 #define CONFIG_FSL_USDHC
-#define CONFIG_SYS_FSL_ESDHC_ADDR      0
-#define CONFIG_SYS_FSL_USDHC_NUM       2
+#define CONFIG_SYS_FSL_ESDHC_ADDR   0
+#define CONFIG_SYS_FSL_USDHC_NUM    2
 
 #define CONFIG_MMC
 #define CONFIG_CMD_MMC
@@ -80,7 +82,7 @@
 #define CONFIG_CMD_EXT2
 #define CONFIG_CMD_FAT
 #define CONFIG_DOS_PARTITION
-#define CONFIG_SYS_MMC_ENV_PART                0      /* user partition */
+#define CONFIG_SYS_MMC_ENV_PART     0      /* user partition */
 
 //#define CONFIG_CMD_SATA
 /*
@@ -88,30 +90,32 @@
  */
 #ifdef CONFIG_CMD_SATA
 #define CONFIG_DWC_AHSATA
-#define CONFIG_SYS_SATA_MAX_DEVICE	1
-#define CONFIG_DWC_AHSATA_PORT_ID	0
-#define CONFIG_DWC_AHSATA_BASE_ADDR	SATA_ARB_BASE_ADDR
+#define CONFIG_SYS_SATA_MAX_DEVICE  1
+#define CONFIG_DWC_AHSATA_PORT_ID   0
+#define CONFIG_DWC_AHSATA_BASE_ADDR SATA_ARB_BASE_ADDR
 #define CONFIG_LBA48
 #define CONFIG_LIBATA
 #endif
 
-//#define CONFIG_CMD_PING
-//#define CONFIG_CMD_DHCP
+//#define CONFIG_FEC_MXC
+#if defined(CONFIG_FEC_MXC)
+#define CONFIG_CMD_PING
+#define CONFIG_CMD_DHCP
 #define CONFIG_CMD_MII
 #define CONFIG_CMD_NET
-#define CONFIG_FEC_MXC
 #define CONFIG_MII
-#define IMX_FEC_BASE			ENET_BASE_ADDR
-#define CONFIG_FEC_XCV_TYPE		RGMII
-#define CONFIG_ETHPRIME			"FEC"
-#define CONFIG_FEC_MXC_PHYADDR		4
+#define IMX_FEC_BASE                ENET_BASE_ADDR
+#define CONFIG_FEC_XCV_TYPE         RGMII
+#define CONFIG_ETHPRIME             "FEC"
+#define CONFIG_FEC_MXC_PHYADDR      4
 #define CONFIG_PHYLIB
 #define CONFIG_PHY_ATHEROS
 
-#define CONFIG_NETMASK		255.255.255.0
-#define CONFIG_IPADDR		192.168.1.102 
-#define CONFIG_SERVERIP    	_SERVER_IP_ADDR_     
-#define CONFIG_GATEWAYIP	_GATEWAY_IP_ADDR_
+#define CONFIG_NETMASK              255.255.255.0
+#define CONFIG_IPADDR               192.168.1.102
+#define CONFIG_SERVERIP             _SERVER_IP_ADDR_
+#define CONFIG_GATEWAYIP            _GATEWAY_IP_ADDR_
+#endif
 
 /* USB Configs */
 #define CONFIG_CMD_USB
@@ -122,42 +126,44 @@
 #define CONFIG_USB_HOST_ETHER
 #define CONFIG_USB_ETHER_ASIX
 #define CONFIG_USB_ETHER_SMSC95XX
-#define CONFIG_MXC_USB_PORT	1
-#define CONFIG_MXC_USB_PORTSC	(PORT_PTS_UTMI | PORT_PTS_PTW)
-#define CONFIG_MXC_USB_FLAGS	0
+#define CONFIG_MXC_USB_PORT         1
+#define CONFIG_MXC_USB_PORTSC       (PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_MXC_USB_FLAGS        0
 
 /* Miscellaneous commands */
 #define CONFIG_CMD_BMODE
 
 /* Framebuffer and LCD */
 //#define CONFIG_VIDEO
-//#define CONFIG_VIDEO_IPUV3
-//#define CONFIG_CFB_CONSOLE
-//#define CONFIG_VGA_AS_SINGLE_DEVICE
-//#define CONFIG_SYS_CONSOLE_IS_IN_ENV
-//#define CONFIG_SYS_CONSOLE_OVERWRITE_ROUTINE
-//#define CONFIG_VIDEO_BMP_RLE8
-//#define CONFIG_SPLASH_SCREEN
-//#define CONFIG_BMP_16BPP
-//#define CONFIG_VIDEO_LOGO
-//#define CONFIG_IPUV3_CLK 260000000
+#if defined(CONFIG_VIDEO)
+#define CONFIG_VIDEO_IPUV3
+#define CONFIG_CFB_CONSOLE
+#define CONFIG_VGA_AS_SINGLE_DEVICE
+#define CONFIG_SYS_CONSOLE_IS_IN_ENV
+#define CONFIG_SYS_CONSOLE_OVERWRITE_ROUTINE
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_BMP_16BPP
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_IPUV3_CLK            260000000
+#endif
 
 /* allow to overwrite serial and ethaddr */
 #define CONFIG_ENV_OVERWRITE
-#define CONFIG_CONS_INDEX	       1
-#define CONFIG_BAUDRATE		 115200
+#define CONFIG_CONS_INDEX           1
+#define CONFIG_BAUDRATE             115200
 
 /* Command definition */
 #include <config_cmd_default.h>
 
 #undef CONFIG_CMD_IMLS
 
-#define CONFIG_BOOTDELAY	       2
+#define CONFIG_BOOTDELAY            2
 
-#define CONFIG_PREBOOT                 ""
+#define CONFIG_PREBOOT              ""
 
-#define CONFIG_LOADADDR			       0x12000000
-#define CONFIG_SYS_TEXT_BASE	       0x17800000
+#define CONFIG_LOADADDR             0x12000000
+#define CONFIG_SYS_TEXT_BASE        0x17800000
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
     "script=boot.scr\0" \
@@ -201,9 +207,9 @@
 /* Miscellaneous configurable options */
 #define CONFIG_SYS_LONGHELP
 #define CONFIG_SYS_HUSH_PARSER
-#define CONFIG_SYS_PROMPT	       "MX6Q_BARS3000 Strim U-Boot> "
+#define CONFIG_SYS_PROMPT           "MX6Q_BARS3000 Strim U-Boot> "
 #define CONFIG_AUTO_COMPLETE
-#define CONFIG_SYS_CBSIZE	       1024
+#define CONFIG_SYS_CBSIZE           1024
 
 /* OCOTP Config */
 #define CONFIG_CMD_FUSE
@@ -212,51 +218,49 @@
 #endif
 
 /* Print Buffer Size */
-#define CONFIG_SYS_PBSIZE (CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
-#define CONFIG_SYS_MAXARGS	       16
-#define CONFIG_SYS_BARGSIZE CONFIG_SYS_CBSIZE
+#define CONFIG_SYS_PBSIZE           (CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
+#define CONFIG_SYS_MAXARGS          16
+#define CONFIG_SYS_BARGSIZE         CONFIG_SYS_CBSIZE
 
-#define CONFIG_SYS_MEMTEST_START       0x10000000
-#define CONFIG_SYS_MEMTEST_END	       0x10010000
-#define CONFIG_SYS_MEMTEST_SCRATCH     0x10800000
+#define CONFIG_SYS_MEMTEST_START    0x10000000
+#define CONFIG_SYS_MEMTEST_END      0x10010000
+#define CONFIG_SYS_MEMTEST_SCRATCH  0x10800000
 
-#define CONFIG_SYS_LOAD_ADDR	       CONFIG_LOADADDR
-#define CONFIG_SYS_HZ		       1000
+#define CONFIG_SYS_LOAD_ADDR        CONFIG_LOADADDR
+#define CONFIG_SYS_HZ               1000
 
 #define CONFIG_CMDLINE_EDITING
 
 /* Physical Memory Map */
-#define CONFIG_NR_DRAM_BANKS	       1
-#define PHYS_SDRAM		       MMDC0_ARB_BASE_ADDR
-#define PHYS_SDRAM_SIZE			       (1u * 1024 * 1024 * 1024)
+#define CONFIG_NR_DRAM_BANKS        1
+#define PHYS_SDRAM                  MMDC0_ARB_BASE_ADDR
+#define PHYS_SDRAM_SIZE             (1u * 1024 * 1024 * 1024)
 
-#define CONFIG_SYS_SDRAM_BASE	       PHYS_SDRAM
-#define CONFIG_SYS_INIT_RAM_ADDR       IRAM_BASE_ADDR
-#define CONFIG_SYS_INIT_RAM_SIZE       IRAM_SIZE
+#define CONFIG_SYS_SDRAM_BASE       PHYS_SDRAM
+#define CONFIG_SYS_INIT_RAM_ADDR    IRAM_BASE_ADDR
+#define CONFIG_SYS_INIT_RAM_SIZE    IRAM_SIZE
 
-#define CONFIG_SYS_INIT_SP_OFFSET \
-       (CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
-#define CONFIG_SYS_INIT_SP_ADDR \
-       (CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+#define CONFIG_SYS_INIT_SP_OFFSET   (CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR     (CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
 
 /* FLASH and environment organization */
 #define CONFIG_SYS_NO_FLASH
 
-#define CONFIG_ENV_SIZE			(8 * 1024)
+#define CONFIG_ENV_SIZE             (8 * 1024)
 
 #define CONFIG_ENV_IS_IN_MMC
 /* #define CONFIG_ENV_IS_IN_SPI_FLASH */
 
 #if defined(CONFIG_ENV_IS_IN_MMC)
-#define CONFIG_ENV_OFFSET		(512 * 1024)
-#define CONFIG_SYS_MMC_ENV_DEV		1   // FIXME было 1
+#define CONFIG_ENV_OFFSET           (512 * 1024)
+#define CONFIG_SYS_MMC_ENV_DEV      1
 #elif defined(CONFIG_ENV_IS_IN_SPI_FLASH)
-#define CONFIG_ENV_OFFSET		(768 * 1024)
-#define CONFIG_ENV_SECT_SIZE		(8 * 1024)
-#define CONFIG_ENV_SPI_BUS		CONFIG_SF_DEFAULT_BUS
-#define CONFIG_ENV_SPI_CS		CONFIG_SF_DEFAULT_CS
-#define CONFIG_ENV_SPI_MODE		CONFIG_SF_DEFAULT_MODE
-#define CONFIG_ENV_SPI_MAX_HZ		CONFIG_SF_DEFAULT_SPEED
+#define CONFIG_ENV_OFFSET           (768 * 1024)
+#define CONFIG_ENV_SECT_SIZE        (8 * 1024)
+#define CONFIG_ENV_SPI_BUS          CONFIG_SF_DEFAULT_BUS
+#define CONFIG_ENV_SPI_CS           CONFIG_SF_DEFAULT_CS
+#define CONFIG_ENV_SPI_MODE         CONFIG_SF_DEFAULT_MODE
+#define CONFIG_ENV_SPI_MAX_HZ       CONFIG_SF_DEFAULT_SPEED
 #endif
 
 #define CONFIG_OF_LIBFDT
