320c277d241589a603da0f7a3a1d913359166d02
Grommerin <grommerin@gmail.com>
2016-07-15 15:42:09 +0300

    Добавлены директивы условной компиляции при отключении некоторых функций устройства. Возвращены настройки UART3-5 (требуется проверка правильности выбора выводов). Добавлена стартовая инициализация светодиодов. Убрана инициализация кнопок. Исправлены найстройки пинов для WEIM.

--- board/strim/mx6q_bars3000/mx6q_bars3000.c
+++ board/strim/mx6q_bars3000/mx6q_bars3000.c
@@ -53,9 +53,11 @@ DECLARE_GLOBAL_DATA_PTR;
 	PAD_CTL_PUS_47K_UP  | PAD_CTL_SPEED_LOW |	       \
 	PAD_CTL_DSE_80ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
 
+#ifdef CONFIG_FEC_MXC
 #define ENET_PAD_CTRL  (PAD_CTL_PKE | PAD_CTL_PUE |		\
 	PAD_CTL_PUS_100K_UP | PAD_CTL_SPEED_MED	  |		\
 	PAD_CTL_DSE_40ohm   | PAD_CTL_HYS)
+#endif
 
 #ifdef CONFIG_SYS_USE_EIMNOR
 #define WEIM_NOR_PAD_CTRL (PAD_CTL_PKE | PAD_CTL_PUE |      \
@@ -69,14 +71,12 @@ DECLARE_GLOBAL_DATA_PTR;
 	PAD_CTL_DSE_40ohm     | PAD_CTL_SRE_FAST)
 #endif
 
-#define BUTTON_PAD_CTRL (PAD_CTL_PKE | PAD_CTL_PUE |		\
-	PAD_CTL_PUS_100K_UP | PAD_CTL_SPEED_MED   |		\
-	PAD_CTL_DSE_40ohm   | PAD_CTL_HYS)
-
+#ifdef CONFIG_I2C_MXC
 #define I2C_PAD_CTRL	(PAD_CTL_PKE | PAD_CTL_PUE |		\
 	PAD_CTL_PUS_100K_UP | PAD_CTL_SPEED_MED |		\
 	PAD_CTL_DSE_40ohm | PAD_CTL_HYS |			\
 	PAD_CTL_ODE | PAD_CTL_SRE_FAST)
+#endif
 
 int dram_init(void)
 {
@@ -95,21 +95,23 @@ iomux_v3_cfg_t const uart2_pads[] = {
 	MX6_PAD_EIM_D27__UART2_RXD | MUX_PAD_CTRL(UART_PAD_CTRL),
 };
 
-//iomux_v3_cfg_t const uart3_pads[] = {
-//    MX6_PAD_EIM_D25__UART3_RXD | MUX_PAD_CTRL(UART_PAD_CTRL),
-//    MX6_PAD_EIM_D24__UART3_TXD | MUX_PAD_CTRL(UART_PAD_CTRL),
-//};
-//
-//iomux_v3_cfg_t const uart4_pads[] = {
-//    MX6_PAD_KEY_COL0__UART4_TXD | MUX_PAD_CTRL(UART_PAD_CTRL),
-//    MX6_PAD_KEY_ROW0__UART4_RXD | MUX_PAD_CTRL(UART_PAD_CTRL),
-//};
-//
-//iomux_v3_cfg_t const uart5_pads[] = {
-//    MX6_PAD_KEY_COL1__UART5_TXD | MUX_PAD_CTRL(UART_PAD_CTRL),
-//    MX6_PAD_KEY_ROW1__UART5_RXD | MUX_PAD_CTRL(UART_PAD_CTRL),
-//};
+iomux_v3_cfg_t const uart3_pads[] = {
+    MX6_PAD_EIM_D25__UART3_RXD | MUX_PAD_CTRL(UART_PAD_CTRL),
+    MX6_PAD_EIM_D24__UART3_TXD | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
 
+iomux_v3_cfg_t const uart4_pads[] = {
+    MX6_PAD_KEY_COL0__UART4_TXD | MUX_PAD_CTRL(UART_PAD_CTRL),
+    MX6_PAD_KEY_ROW0__UART4_RXD | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+iomux_v3_cfg_t const uart5_pads[] = {
+    MX6_PAD_KEY_COL1__UART5_TXD | MUX_PAD_CTRL(UART_PAD_CTRL),
+    MX6_PAD_KEY_ROW1__UART5_RXD | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+
+#if defined(CONFIG_I2C_MXC)
 #define PC MUX_PAD_CTRL(I2C_PAD_CTRL)
 
 /* I2C1, SGTL5000 */
@@ -153,6 +155,8 @@ struct i2c_pads_info i2c_pad_info2 = {
 		.gp = IMX_GPIO_NR(7, 11)
 	}
 };
+#endif
+
 
 iomux_v3_cfg_t const usdhc2_pads[] = {
         MX6_PAD_SD2_CLK__USDHC2_CLK     | MUX_PAD_CTRL(USDHC_PAD_CTRL),
@@ -186,6 +190,15 @@ iomux_v3_cfg_t const usdhc4_pads[] = {
 	MX6_PAD_SD4_DAT7__USDHC4_DAT7 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
 };
 
+iomux_v3_cfg_t const led_pads[] = {
+    MX6_PAD_EIM_D17__GPIO_3_17    | MUX_PAD_CTRL(NO_PAD_CTRL),    /* LED_WiFi */
+    MX6_PAD_EIM_D18__GPIO_3_18    | MUX_PAD_CTRL(NO_PAD_CTRL),    /* LED_Power/Work */
+    MX6_PAD_EIM_D16__GPIO_3_16    | MUX_PAD_CTRL(NO_PAD_CTRL),    /* LED_CAN2 */
+    MX6_PAD_KEY_COL2__GPIO_4_10   | MUX_PAD_CTRL(NO_PAD_CTRL),    /* LED_CAN1 */
+    MX6_PAD_SD3_RST__GPIO_7_8     | MUX_PAD_CTRL(NO_PAD_CTRL),    /* LED_GPS/GPRS */
+};
+
+#if defined(CONFIG_FEC_MXC)
 iomux_v3_cfg_t const enet_pads1[] = {
 	MX6_PAD_ENET_MDIO__ENET_MDIO		| MUX_PAD_CTRL(ENET_PAD_CTRL),
 	MX6_PAD_ENET_MDC__ENET_MDC		| MUX_PAD_CTRL(ENET_PAD_CTRL),
@@ -208,22 +221,6 @@ iomux_v3_cfg_t const enet_pads2[] = {
 	MX6_PAD_RGMII_RX_CTL__RGMII_RX_CTL	| MUX_PAD_CTRL(ENET_PAD_CTRL),
 };
 
-/* Button assignments for J14 */
-static iomux_v3_cfg_t const button_pads[] = {
-	/* Menu */
-	MX6_PAD_NANDF_D1__GPIO_2_1	| MUX_PAD_CTRL(BUTTON_PAD_CTRL),
-	/* Back */
-	MX6_PAD_NANDF_D2__GPIO_2_2	| MUX_PAD_CTRL(BUTTON_PAD_CTRL),
-	/* Labelled Search (mapped to Power under Android) */
-	MX6_PAD_NANDF_D3__GPIO_2_3	| MUX_PAD_CTRL(BUTTON_PAD_CTRL),
-	/* Home */
-	MX6_PAD_NANDF_D4__GPIO_2_4	| MUX_PAD_CTRL(BUTTON_PAD_CTRL),
-	/* Volume Down */
-	MX6_PAD_GPIO_19__GPIO_4_5	| MUX_PAD_CTRL(BUTTON_PAD_CTRL),
-	/* Volume Up */
-	MX6_PAD_GPIO_18__GPIO_7_13	| MUX_PAD_CTRL(BUTTON_PAD_CTRL),
-};
-
 iomux_v3_cfg_t const enet_pads[] = {
         MX6_PAD_ENET_MDIO__ENET_MDIO            | MUX_PAD_CTRL(ENET_PAD_CTRL),
         MX6_PAD_ENET_MDC__ENET_MDC              | MUX_PAD_CTRL(ENET_PAD_CTRL),
@@ -243,26 +240,11 @@ iomux_v3_cfg_t const enet_pads[] = {
         /* AR8035 PHY Reset */
 	 MX6_PAD_EIM_D31__GPIO_3_31              | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
+#endif
 
 
 #ifdef CONFIG_SYS_USE_EIMNOR
 iomux_v3_cfg_t eimnor_pads[] = {
-    MX6_PAD_EIM_D16__WEIM_WEIM_D_16      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D17__WEIM_WEIM_D_17      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D18__WEIM_WEIM_D_18      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D19__WEIM_WEIM_D_19      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D20__WEIM_WEIM_D_20      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D21__WEIM_WEIM_D_21      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D22__WEIM_WEIM_D_22      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D23__WEIM_WEIM_D_23      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D24__WEIM_WEIM_D_24      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D25__WEIM_WEIM_D_25      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D26__WEIM_WEIM_D_26      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D27__WEIM_WEIM_D_27      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D28__WEIM_WEIM_D_28      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D29__WEIM_WEIM_D_29      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D30__WEIM_WEIM_D_30      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
-    MX6_PAD_EIM_D31__WEIM_WEIM_D_31      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
     MX6_PAD_EIM_DA0__WEIM_WEIM_DA_A_0    | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
     MX6_PAD_EIM_DA1__WEIM_WEIM_DA_A_1    | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
     MX6_PAD_EIM_DA2__WEIM_WEIM_DA_A_2    | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
@@ -287,12 +269,24 @@ iomux_v3_cfg_t eimnor_pads[] = {
     MX6_PAD_EIM_A21__WEIM_WEIM_A_21      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
     MX6_PAD_EIM_A22__WEIM_WEIM_A_22      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
     MX6_PAD_EIM_A23__WEIM_WEIM_A_23      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
+    MX6_PAD_EIM_A24__WEIM_WEIM_A_24      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
+    MX6_PAD_EIM_A25__WEIM_WEIM_A_25      | MUX_PAD_CTRL(WEIM_NOR_PAD_CTRL),
     MX6_PAD_EIM_OE__WEIM_WEIM_OE         | MUX_PAD_CTRL(NO_PAD_CTRL),
     MX6_PAD_EIM_RW__WEIM_WEIM_RW         | MUX_PAD_CTRL(NO_PAD_CTRL),
     MX6_PAD_EIM_CS0__WEIM_WEIM_CS_0      | MUX_PAD_CTRL(NO_PAD_CTRL),
-    /* Steer logic */
-    MX6_PAD_EIM_A24__GPIO_5_4            | MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX6_PAD_EIM_CS1__WEIM_WEIM_CS_1      | MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX6_PAD_EIM_OE__WEIM_WEIM_OE         | MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX6_PAD_EIM_RW__WEIM_WEIM_RW         | MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX6_PAD_EIM_LBA__WEIM_WEIM_LBA       | MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX6_PAD_EIM_WAIT__WEIM_WEIM_WAIT     | MUX_PAD_CTRL(NO_PAD_CTRL),
+//
+//    /* Steer logic */
+//    MX6_PAD_EIM_A24__GPIO_5_4            | MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX6_PAD_EIM_EB1__GPIO_2_29           | MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX6_PAD_EIM_EB2__GPIO_2_30           | MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX6_PAD_EIM_EB3__GPIO_2_31           | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
+
 static void eimnor_cs_setup(void)
 {
     writel(0x00000120, WEIM_BASE_ADDR + 0x090);
@@ -308,13 +302,19 @@ static void setup_eimnor(void)
     imx_iomux_v3_setup_multiple_pads(eimnor_pads,
             ARRAY_SIZE(eimnor_pads));
 
-    gpio_direction_output(IMX_GPIO_NR(5, 4), 0);
+    gpio_direction_output(IMX_GPIO_NR(2, 29), 0);
+    gpio_direction_output(IMX_GPIO_NR(2, 30), 0);
+    gpio_direction_output(IMX_GPIO_NR(2, 31), 0);
+    mdelay(2);
+    gpio_set_value(IMX_GPIO_NR(2, 29), 1);
+    gpio_set_value(IMX_GPIO_NR(2, 30), 1);
+    gpio_set_value(IMX_GPIO_NR(2, 31), 1);
 
     eimnor_cs_setup();
 }
 #endif
 
-
+#if defined(CONFIG_FEC_MXC)
 static void setup_iomux_enet(void)
 {
         imx_iomux_v3_setup_multiple_pads(enet_pads, ARRAY_SIZE(enet_pads));
@@ -324,6 +324,7 @@ static void setup_iomux_enet(void)
         udelay(500);
         gpio_set_value(IMX_GPIO_NR(3, 31), 1);
 }
+#endif
 
 iomux_v3_cfg_t const usb_pads[] = {
 	MX6_PAD_GPIO_17__GPIO_7_12 | MUX_PAD_CTRL(NO_PAD_CTRL),
@@ -431,6 +432,7 @@ void setup_spi(void)
 }
 #endif
 
+#if defined(CONFIG_FEC_MXC)
 int mx6_rgmii_rework(struct phy_device *phydev)
 {
         unsigned short val, id1, id2;
@@ -493,11 +495,36 @@ int board_eth_init(bd_t *bis)
 
         return 0;
 }
+#endif
 
-static void setup_buttons(void)
+static void setup_leds(void)
 {
-	imx_iomux_v3_setup_multiple_pads(button_pads,
-					 ARRAY_SIZE(button_pads));
+    imx_iomux_v3_setup_multiple_pads(led_pads,
+                     ARRAY_SIZE(led_pads));
+
+    /* LED_WiFi */
+    gpio_direction_output(IMX_GPIO_NR(3, 17), 0);
+    /* LED_Power/Work */
+    gpio_direction_output(IMX_GPIO_NR(3, 18), 0);
+    /* LED_CAN2 */
+    gpio_direction_output(IMX_GPIO_NR(3, 16), 0);
+    /* LED_CAN1 */
+    gpio_direction_output(IMX_GPIO_NR(4, 10), 0);
+    /* LED_GPS/GPRS */
+    gpio_direction_output(IMX_GPIO_NR(7, 8), 0);
+
+    mdelay(2);
+
+    /* LED_WiFi */
+    gpio_set_value(IMX_GPIO_NR(3, 17), 1);
+    /* LED_Power/Work */
+    gpio_set_value(IMX_GPIO_NR(3, 18), 1);
+    /* LED_CAN2 */
+    gpio_set_value(IMX_GPIO_NR(3, 16), 1);
+    /* LED_CAN1 */
+    gpio_set_value(IMX_GPIO_NR(4, 10), 1);
+    /* LED_GPS/GPRS */
+    gpio_set_value(IMX_GPIO_NR(7, 8), 1);
 }
 
 #ifdef CONFIG_CMD_SATA
@@ -834,11 +861,13 @@ static void setup_display(void)
 int board_early_init_f(void)
 {
 	setup_iomux_uart();
-	setup_buttons();
+	setup_leds();
 
 #if defined(CONFIG_VIDEO_IPUV3)
 	setup_display();
 #endif
+
+	puts("test3\n");
 	return 0;
 }
 
@@ -848,6 +877,7 @@ int board_early_init_f(void)
  */
 int overwrite_console(void)
 {
+    puts("test2\n");
 	return 1;
 }
 
@@ -859,9 +889,12 @@ int board_init(void)
 #ifdef CONFIG_MXC_SPI
 	setup_spi();
 #endif
-//	setup_i2c(0, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info0);
-//	setup_i2c(1, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info1);
-//	setup_i2c(2, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info2);
+
+#if defined(CONFIG_SYS_I2C_SPEED)
+	setup_i2c(0, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info0);
+	setup_i2c(1, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info1);
+	setup_i2c(2, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info2);
+#endif
 
 #ifdef CONFIG_SYS_USE_EIMNOR
     setup_eimnor();
@@ -870,6 +903,8 @@ int board_init(void)
 #ifdef CONFIG_CMD_SATA
 	setup_sata();
 #endif
+
+	puts("test1\n");
 
 	return 0;
 }
@@ -880,89 +915,6 @@ int checkboard(void)
 
 	return 0;
 }
-
-struct button_key {
-	char const	*name;
-	unsigned	gpnum;
-	char		ident;
-};
-
-static struct button_key const buttons[] = {
-	{"back",	IMX_GPIO_NR(2, 2),	'B'},
-	{"home",	IMX_GPIO_NR(2, 4),	'H'},
-	{"menu",	IMX_GPIO_NR(2, 1),	'M'},
-	{"search",	IMX_GPIO_NR(2, 3),	'S'},
-	{"volup",	IMX_GPIO_NR(7, 13),	'V'},
-	{"voldown",	IMX_GPIO_NR(4, 5),	'v'},
-};
-
-/*
- * generate a null-terminated string containing the buttons pressed
- * returns number of keys pressed
- */
-static int read_keys(char *buf)
-{
-	int i, numpressed = 0;
-	for (i = 0; i < ARRAY_SIZE(buttons); i++) {
-		if (!gpio_get_value(buttons[i].gpnum))
-			buf[numpressed++] = buttons[i].ident;
-	}
-	buf[numpressed] = '\0';
-	return numpressed;
-}
-
-static int do_kbd(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
-{
-	char envvalue[ARRAY_SIZE(buttons)+1];
-	int numpressed = read_keys(envvalue);
-	setenv("keybd", envvalue);
-	return numpressed == 0;
-}
-
-U_BOOT_CMD(
-	kbd, 1, 1, do_kbd,
-	"Tests for keypresses, sets 'keybd' environment variable",
-	"Returns 0 (true) to shell if key is pressed."
-);
-
-#ifdef CONFIG_PREBOOT
-static char const kbd_magic_prefix[] = "key_magic";
-static char const kbd_command_prefix[] = "key_cmd";
-
-static void preboot_keys(void)
-{
-	int numpressed;
-	char keypress[ARRAY_SIZE(buttons)+1];
-	numpressed = read_keys(keypress);
-	if (numpressed) {
-		char *kbd_magic_keys = getenv("magic_keys");
-		char *suffix;
-		/*
-		 * loop over all magic keys
-		 */
-		for (suffix = kbd_magic_keys; *suffix; ++suffix) {
-			char *keys;
-			char magic[sizeof(kbd_magic_prefix) + 1];
-			sprintf(magic, "%s%c", kbd_magic_prefix, *suffix);
-			keys = getenv(magic);
-			if (keys) {
-				if (!strcmp(keys, keypress))
-					break;
-			}
-		}
-		if (*suffix) {
-			char cmd_name[sizeof(kbd_command_prefix) + 1];
-			char *cmd;
-			sprintf(cmd_name, "%s%c", kbd_command_prefix, *suffix);
-			cmd = getenv(cmd_name);
-			if (cmd) {
-				setenv("preboot", cmd);
-				return;
-			}
-		}
-	}
-}
-#endif
 
 #ifdef CONFIG_CMD_BMODE
 static const struct boot_mode board_boot_modes[] = {
@@ -975,10 +927,6 @@ static const struct boot_mode board_boot_modes[] = {
 
 int misc_init_r(void)
 {
-#ifdef CONFIG_PREBOOT
-	preboot_keys();
-#endif
-
 #ifdef CONFIG_CMD_BMODE
 	add_board_boot_modes(board_boot_modes);
 #endif
